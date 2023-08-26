8c37c5b52e4cb3b4a9d920d9bde247e03b937c8e
Carlos A. Rangel <carange4@asu.edu>
2023-08-26 15:22:29 -0700

    Add project files.

---
+++ IService1.cs
@@ -0,0 +1,17 @@
+﻿using System;               // for array class
+using System.Linq;          // for sorting algorithms
+using System.ServiceModel;
+
+namespace Assignment1 {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface TempAndSorting {
+
+        [OperationContract]
+        int c2f(int c);
+        [OperationContract]
+        int f2c(int f);
+        [OperationContract]
+        string sort(string s);
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Assignment1 {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : TempAndSorting {
+        //basic temp conversions (returning integers, which truncate the floating point values)
+        public int c2f(int c) {
+            return (int)((c * (9.0 / 5.0)) + 32.0);
+        }
+
+        public int f2c(int f) {
+            return (int)((f - 32.0) * (5.0 / 9.0));
+        }
+
+        // sorts a given string of numbers by transforming it into an integer array, 
+        // sorting that, and transforming that sorted array back into a string. 
+        public string sort(string s) {
+            string[] numsInString = s.Split(',');
+            int[] nums = Array.ConvertAll(numsInString, int.Parse);
+            int[] sortedArr = nums.OrderBy(x => x).ToArray();
+            string sortedString = string.Join(",", sortedArr);
+            return sortedString;
+        }
+    }
+}
